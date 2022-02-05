7e86ac9d8732e7c54344bf2c9d02e9a77d5c744f
Ali Adel <42678489+aaesmail@users.noreply.github.com>
2022-02-05 12:25:44 +0200

    Remove global http object

--- Manhwa.py
+++ Manhwa.py
@@ -23,8 +23,6 @@ dynamic_pages_lock = threading.Lock()
 unread_manhwa = []
 unread_manhwa_lock = threading.Lock()
 
-http = urllib3.PoolManager(num_pools=100)
-
 def main():
     global history
     history = get_chrome_history()
@@ -89,8 +87,7 @@ def append_unread_chapter(order, manhwa):
         append_failed_manhwa(order, manhwa)
 
 def get_webpage(url):
-    global http
-    page = http.request('GET', url, timeout=60)
+    page = urllib3.PoolManager().request('GET', url, timeout=60)
     return BeautifulSoup(page.data, 'html.parser')
 
 def get_urls(webpage):
